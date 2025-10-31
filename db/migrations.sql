f38a4614605dae0af4d185d103910b3a6ad7543c
Laura Miron <laurabmiron@gmail.com>
2025-10-30 22:12:20 -0700

    drop tables in reverse dependency order

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,15 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-DROP TABLE IF EXISTS streaming_service;
+-- Drop tables in reverse dependency order
+DROP TABLE IF EXISTS session_states;
+DROP TABLE IF EXISTS streaming_accounts;
+DROP TABLE IF EXISTS streaming_service CASCADE;
 
-CREATE TABLE IF NOT EXISTS streaming_service (
+CREATE TABLE streaming_service (
   id SERIAL PRIMARY KEY,
   name TEXT UNIQUE NOT NULL  -- 'netflix', 'prime', 'hulu'...
 );
 
-DROP TABLE IF EXISTS streaming_accounts;
-
-CREATE TABLE IF NOT EXISTS streaming_accounts (
+CREATE TABLE streaming_accounts (
     id SERIAL PRIMARY KEY,
     streaming_service_id INT NOT NULL REFERENCES streaming_service(id),
     email TEXT NOT NULL,
@@ -19,9 +20,7 @@ CREATE TABLE IF NOT EXISTS streaming_accounts (
     -- updated_at TIMESTAMP DEFAULT now()
 );
 
-DROP TABLE IF EXISTS session_states;
-
-CREATE TABLE IF NOT EXISTS session_states (
+CREATE TABLE session_states (
     id SERIAL PRIMARY KEY,
     streaming_service_id INT UNIQUE NOT NULL REFERENCES streaming_service(id),
     json_state JSONB NOT NULL,       -- serialized Playwright storageState()
